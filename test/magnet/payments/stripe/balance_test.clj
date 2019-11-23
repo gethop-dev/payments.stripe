4270fea72db45a3b8ccadd9d300012aacf135f8b
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2019-11-23 13:13:03 +0100

    Use the corrent license file and add MPL 2.0 headers
    We inadvertedly left the EPL 1.0 license file that gets added by
    default when you create a library using Leiningen, but the license of
    the library is the MPL 2.0 (as stated in the README.md file).
    
    So remove the EPL license file, add the correct MPL 2.0 license file
    and add all the missing MPL 2.0 headers to all code files.

--- test/magnet/payments/stripe/balance_test.clj
+++ test/magnet/payments/stripe/balance_test.clj
@@ -1,3 +1,7 @@
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/
+
 (ns magnet.payments.stripe.balance-test
   (:require [clojure.test :refer :all]
             [integrant.core :as ig]
