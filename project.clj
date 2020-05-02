8cd9179ff78d34b88b693a76bc769fd7daf9703d
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2020-05-02 02:05:52 +0200

    Bump dependencies to newer versions

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
             :url "https://www.mozilla.org/en-US/2.0/"}
   :min-lein-version "2.9.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [integrant "0.7.0"]
+                 [integrant "0.8.0"]
                  [http-kit "2.3.0"]
-                 [diehard "0.8.5"]
-                 [org.clojure/data.json "0.2.6"]]
+                 [diehard "0.9.4"]
+                 [org.clojure/data.json "1.0.0"]]
   :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
                                       :username :env/clojars_username
                                       :password :env/clojars_password
@@ -19,8 +19,8 @@
                                       :sign-releases false}]]
   :profiles {:dev [:project/dev :profiles/dev]
              :profiles/dev {}
-             :project/dev {:plugins [[jonase/eastwood "0.3.4"]
-                                     [lein-cljfmt "0.6.2"]]}
+             :project/dev {:plugins [[jonase/eastwood "0.3.11"]
+                                     [lein-cljfmt "0.6.7"]]}
              :repl {:repl-options {:init-ns magnet.payments.stripe
                                    :host "0.0.0.0"
                                    :port 4001}}})
