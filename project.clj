2e4efc949956d3b3f1fb132eae1997a74eea11c4
Bingen Galartza Iparragirre <bingen.galartza@biotz.io>
2025-02-21 10:35:21 +0100

    Bump dependencies

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Mozilla Public License 2.0"
             :url "https://www.mozilla.org/en-US/2.0/"}
   :min-lein-version "2.9.8"
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [integrant "0.8.1"]
-                 [http-kit "2.7.0"]
+                 [http-kit/http-kit "2.8.0"]
                  [diehard "0.11.12"]
-                 [org.clojure/data.json "2.5.0"]
-                 [com.stripe/stripe-java "24.20.0"]]
+                 [org.clojure/data.json "2.5.1"]
+                 [com.stripe/stripe-java "28.3.1"]]
   :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
                                       :username :env/CLOJARS_USERNAME
                                       :password :env/CLOJARS_PASSWORD
@@ -20,8 +20,8 @@
                                       :sign-releases false}]]
   :profiles {:dev [:project/dev :profiles/dev]
              :profiles/dev {}
-             :project/dev {:plugins [[jonase/eastwood "1.4.2"]
-                                     [dev.weavejester/lein-cljfmt "0.12.0"]]}
+             :project/dev {:plugins [[jonase/eastwood "1.4.3"]
+                                     [dev.weavejester/lein-cljfmt "0.13.0"]]}
              :repl {:repl-options {:init-ns dev.gethop.payments.stripe
                                    :host "0.0.0.0"
                                    :port 4001}}})
